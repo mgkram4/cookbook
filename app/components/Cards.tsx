a49f6e0ebeecac6b551446b86a3da361a07879c7
mgkram18 <kram18.mg@gmail.com>
2023-08-09 16:16:15 -0700

    type error

--- app/components/Cards.tsx
+++ app/components/Cards.tsx
@@ -15,41 +15,37 @@ export default async function Card() {
 
   return (
     <ul>
-      {data.map(
-        (
-          post // Added parentheses around the map function
-        ) => (
-          <li key={post._id}>
-            <div className="card w-96 bg-base-100 shadow-xl">
-              {post.image?.asset && (
-                <Image
-                  className="rounded-xl mt-4"
-                  src={urlFor(post.image).url()}
-                  alt="logos"
-                  width={100}
-                  height={100}
-                  placeholder="blur"
-                  blurDataURL={urlFor(post.image)
-                    .width(24)
-                    .height(24)
-                    .blur(10)
-                    .url()}
-                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 40vw"
-                />
-              )}
-              <div className="card-body">
-                <h2 className="card-title">{post.title}</h2>
-                <p>{post.overview}</p>
-                <div className="card-actions justify-end">
-                  <Link href={`/post/${post.slug.current}`}>
-                    <button className="btn btn-primary">Learn More</button>
-                  </Link>
-                </div>
+      {data.map((post: Post) => (
+        <li key={post._id}>
+          <div className="card w-96 bg-base-100 shadow-xl">
+            {post.image?.asset && (
+              <Image
+                className="rounded-xl mt-4"
+                src={urlFor(post.image).url()}
+                alt="logos"
+                width={100}
+                height={100}
+                placeholder="blur"
+                blurDataURL={urlFor(post.image)
+                  .width(24)
+                  .height(24)
+                  .blur(10)
+                  .url()}
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 40vw"
+              />
+            )}
+            <div className="card-body">
+              <h2 className="card-title">{post.title}</h2>
+              <p>{post.overview}</p>
+              <div className="card-actions justify-end">
+                <Link href={`/post/${post.slug.current}`}>
+                  <button className="btn btn-primary">Learn More</button>
+                </Link>
               </div>
             </div>
-          </li>
-        )
-      )}
+          </div>
+        </li>
+      ))}
     </ul>
   );
 }
